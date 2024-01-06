d2f023b104da0d5510c003f204ed6b0c8a65fbbd
Thato <t.chitja369@gmail.com>
2024-01-06 08:28:32 +0200

    Set the initial HTTP request to the url

--- src/automation.py
+++ src/automation.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Page has been retrieved")
+else:
+    print(f"Failed to retrieve the page. Status code: {response.status_code}")
